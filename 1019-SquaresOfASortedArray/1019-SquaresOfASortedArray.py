ce4ae3d6b83b84743f6277f2b30299bfb95f2e7b
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-08-09 02:26:43 +0530

    Create 1019-SquaresOfASortedArray

---
+++ 1019-SquaresOfASortedArray/1019-SquaresOfASortedArray.py
@@ -0,0 +1,7 @@
+# Last updated: 8/9/2025, 2:26:41 AM
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i in range(0, len(nums)):
+            nums[i] = nums[i]**2
+        nums.sort()
+        return nums
