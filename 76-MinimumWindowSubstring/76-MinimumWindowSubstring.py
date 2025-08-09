7f1bb23914619a6d1118af3dc125118c6f572ba7
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-08-10 02:47:59 +0530

    File updated at 8/10/2025, 2:47:57 AM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
@@ -1,24 +1,31 @@
-# Last updated: 8/10/2025, 2:32:12 AM
+# Last updated: 8/10/2025, 2:47:57 AM
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         from collections import Counter
-        i = 0
-        window = Counter()
-        target = Counter(t)
-        minLen = float('inf')
-        startIndex = 0
 
+        i = 0
+        target = Counter(t)
+        window = {}
+        res = [0, 0]
+        minLen = float('inf')
+        currChar, targetChar = 0, len(target)
         for j in range(len(s)):
-            window[s[j]] += 1
-            while all(window[c] >= target[c] for c in target):
+            window[s[j]] = window.get(s[j], 0) + 1
+
+            if s[j] in target and window[s[j]] == target[s[j]]:
+                currChar += 1
+
+            while currChar == targetChar:
                 if j - i + 1 < minLen:
+                    res = [i, j]
                     minLen = j - i + 1
-                    startIndex = i
-                window[s[i]] -= 1
+                window[s[i]] -=1
+                if s[i] in target and window[s[i]] < target[s[i]]:
+                    currChar -= 1
                 i+=1
-            
-        if minLen != float('inf'):
-            return s[startIndex:startIndex + minLen]
-        else:
-            return ""
+        start, end = res
+        return s[start:end+1] if minLen != float('inf') else ""
+
+
+
 
