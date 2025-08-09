1ee23f5753e8b1684a4d5fa3e0aba9a60cabc1de
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-08-10 03:13:46 +0530

    Create 438-FindAllAnagramsInAString

---
+++ 438-FindAllAnagramsInAString/438-FindAllAnagramsInAString.py
@@ -0,0 +1,23 @@
+# Last updated: 8/10/2025, 3:13:44 AM
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        from collections import Counter
+        i = 0
+        hashMap = set()
+        res = []
+        target = Counter(p)
+        window = Counter()
+
+        for j in range(len(s)):
+            window[s[j]] += 1
+            if j >= len(p):
+                left_char = s[j - len(p)]
+                if window[left_char] == 1:
+                    del window[left_char]
+                else:
+                    window[left_char] -= 1
+            if window == target:
+                res.append(j-len(p)+1)
+        return res
+
+                
