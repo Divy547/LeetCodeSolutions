bb6c28f6a8ea9fd4f79b4c96449236e3818ec0e4
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-08-10 19:12:35 +0530

    File updated at 8/10/2025, 7:12:33 PM

--- 1839-LongestSubstringOfAllVowelsinOrder/1839-LongestSubstringOfAllVowelsinOrder.py
+++ 1839-LongestSubstringOfAllVowelsinOrder/1839-LongestSubstringOfAllVowelsinOrder.py
@@ -1,8 +1,7 @@
-# Last updated: 8/10/2025, 7:11:49 PM
+# Last updated: 8/10/2025, 7:12:33 PM
 class Solution:
     def longestBeautifulSubstring(self, word: str) -> int:
         ranks = {'a': 1, 'e': 2, 'i': 3, 'o': 4, 'u': 5}
-        i = 0
         max_len = 0
         unique_vowels = 1
         prevRank = 1
@@ -10,7 +9,7 @@ class Solution:
         while j < len(word) and word[j] != "a":
             j += 1
         if j == len(word):
-            return 0  # no 'a' found
+            return 0 
         i = j
         
         for j in range(len(word)):
@@ -19,13 +18,10 @@ class Solution:
                 i = j
                 unique_vowels = 1
             
-            # If rank increased, means we found a new vowel in order
             elif currRank > prevRank:
                 unique_vowels += 1
             
             prevRank = currRank
-            
-            # Update max length only if all vowels found
             if unique_vowels == 5:
                 max_len = max(max_len, j - i + 1)
         
