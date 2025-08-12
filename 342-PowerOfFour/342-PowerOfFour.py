fa7f5ae5e02929e82948313d43edc169daad74e2
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-08-13 00:13:56 +0530

    Create 342-PowerOfFour

---
+++ 342-PowerOfFour/342-PowerOfFour.py
@@ -0,0 +1,8 @@
+# Last updated: 8/13/2025, 12:13:52 AM
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n == 1:
+            return True
+        if n < 1 or n % 4!= 0:
+            return False
+        return self.isPowerOfFour(n//4)
