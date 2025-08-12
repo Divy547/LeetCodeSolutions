d253778f7f9abbf75f39e20071678999daa7f382
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-08-13 00:14:13 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.py
@@ -0,0 +1,17 @@
+# Last updated: 8/13/2025, 12:14:09 AM
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        if nums[mid] > target:
+            return mid
+        return mid + 1
