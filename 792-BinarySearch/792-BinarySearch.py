8b8f537ab478a05c91aadd9be3968c8d47383524
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-08-13 00:13:45 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.py
@@ -0,0 +1,14 @@
+# Last updated: 8/13/2025, 12:13:41 AM
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
