ff3ffdf45f6d9595fae361a4b7f5afb6975a195d
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-08-12 22:58:26 +0530

    Create 69-Sqrt(x)

---
+++ 69-Sqrt(x)/69-Sqrt(x).py
@@ -0,0 +1,18 @@
+# Last updated: 8/12/2025, 10:58:23 PM
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        left = 0
+        right = x//2
+        ans = 0
+        while left <= right:
+            mid = (left + right)//2
+            if mid*mid == x:
+                return mid
+            elif mid*mid < x:
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return ans
