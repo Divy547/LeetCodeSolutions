9cb9150ada8afab3d80fdf434906cb0d86d51096
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-08-13 22:42:36 +0530

    File updated at 8/13/2025, 10:42:33 PM

--- 169-MajorityElement/169-MajorityElement.py
+++ 169-MajorityElement/169-MajorityElement.py
@@ -1,12 +1,14 @@
-# Last updated: 8/13/2025, 10:41:48 PM
+# Last updated: 8/13/2025, 10:42:33 PM
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = 0
-        candidate = None
-
-        for num in nums:
-            if count == 0:
-                candidate = num
-            count += (1 if num == candidate else -1)
-
-        return candidate
+        m = nums[0]
+        c = 0
+        for x in nums:
+            if c == 0 :
+                m = x
+                c = 1
+            elif m == x:
+                c += 1
+            else:
+                c -= 1
+        return m
