fac4d1f36658538d1e70ad0bddd65d07a796244d
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-08-13 22:41:52 +0530

    File updated at 8/13/2025, 10:41:48 PM

--- 169-MajorityElement/169-MajorityElement.py
+++ 169-MajorityElement/169-MajorityElement.py
@@ -1,20 +1,12 @@
-# Last updated: 8/13/2025, 10:40:14 PM
+# Last updated: 8/13/2025, 10:41:48 PM
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        def majority(low, high):
-            if low == high:
-                return nums[low]
+        count = 0
+        candidate = None
 
-            mid = (low + high) // 2
-            left_major = majority(low, mid)
-            right_major = majority(mid + 1, high)
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
 
-            if left_major == right_major:
-                return left_major
-
-            left_count = sum(1 for i in range(low, high + 1) if nums[i] == left_major)
-            right_count = sum(1 for i in range(low, high + 1) if nums[i] == right_major)
-
-            return left_major if left_count > right_count else right_major
-
-        return majority(0, len(nums) - 1)
+        return candidate
