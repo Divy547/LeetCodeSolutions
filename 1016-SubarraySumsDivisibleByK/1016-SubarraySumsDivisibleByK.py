9e87b1d0154070e56c4730e6dd9b9ffbf0ff776a
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-08-15 23:43:22 +0530

    Create 1016-SubarraySumsDivisibleByK

---
+++ 1016-SubarraySumsDivisibleByK/1016-SubarraySumsDivisibleByK.py
@@ -0,0 +1,16 @@
+# Last updated: 8/15/2025, 11:43:19 PM
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        prefix_sum=0
+        count=0
+        freq={0:1}
+        for num in nums:
+            prefix_sum+=num
+            rem=prefix_sum%k
+            
+            if rem in freq:
+                count+=freq[rem]
+                freq[rem]+=1
+            else:
+                freq[rem]=1
+        return count
