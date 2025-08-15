c51817f40c7cb0189d2da2539463fc5daed2e98f
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-08-15 23:01:28 +0530

    File updated at 8/15/2025, 11:01:25 PM

--- 974-SubarraySumsDivisiblebyK/974-SubarraySumsDivisiblebyK.py
+++ 974-SubarraySumsDivisiblebyK/974-SubarraySumsDivisiblebyK.py
@@ -1,18 +1,16 @@
-# Last updated: 8/15/2025, 10:59:06 PM
+# Last updated: 8/15/2025, 11:01:25 PM
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        hashMap = {0:1}
-        count = 0
-        currSum = 0
-        for x in nums:
-            currSum += x
-            remainder = currSum % k
-            if remainder < 0:
-                remainder += k 
-            if remainder in hashMap:
-                count += hashMap[remainder]
-            if remainder in hashMap:
-                hashMap[remainder] += 1
+        prefix_sum=0
+        count=0
+        freq={0:1}
+        for num in nums:
+            prefix_sum+=num
+            rem=prefix_sum%k
+            
+            if rem in freq:
+                count+=freq[rem]
+                freq[rem]+=1
             else:
-                hashMap[remainder] = 1
+                freq[rem]=1
         return count
