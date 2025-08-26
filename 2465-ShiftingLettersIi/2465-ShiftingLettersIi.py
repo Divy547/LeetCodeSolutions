6ee1c17a836643d7244b2f4c2e7052eceb6a9bb2
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-08-26 12:59:20 +0530

    Create 2465-ShiftingLettersIi

---
+++ 2465-ShiftingLettersIi/2465-ShiftingLettersIi.py
@@ -0,0 +1,21 @@
+# Last updated: 8/26/2025, 12:59:15 PM
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        diff = [0]*(len(s)+1)
+        for l, r, direction in shifts:
+                if direction == 1:
+                    diff[l] += 1
+                    diff[r+1] -= 1
+                else:
+                    diff[l] -= 1
+                    diff[r+1] += 1
+        for i in range(1, len(diff)):
+            diff[i] = diff[i-1] + diff[i]
+        print(diff[:-1])
+        res = []
+        for i, ch in enumerate(s):
+            base = ord('a')
+            oldval = ord(ch) - base
+            newVal = (oldval + diff[i]) % 26
+            res.append(chr(base + newVal))
+        return "".join(res)
