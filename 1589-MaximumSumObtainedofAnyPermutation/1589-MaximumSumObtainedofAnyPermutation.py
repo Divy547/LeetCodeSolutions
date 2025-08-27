963027ecef0052b66d3216f1f0392fec1d3efd93
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-08-27 22:58:21 +0530

    Create 1589-MaximumSumObtainedofAnyPermutation

---
+++ 1589-MaximumSumObtainedofAnyPermutation/1589-MaximumSumObtainedofAnyPermutation.py
@@ -0,0 +1,22 @@
+# Last updated: 8/27/2025, 10:58:15 PM
+class Solution:
+    def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
+        MOD = 10**9 + 7
+        freq = [0]*(len(nums)+1)
+        for l, r in requests:
+            freq[l] += 1
+            if r + 1 < len(freq):
+                freq[r + 1] -= 1
+        for i in range(1, len(nums)):
+            freq[i] += freq[i-1]
+
+        freq = freq[:-1]
+
+        nums.sort(reverse=True)
+        freq.sort(reverse=True)
+
+        ans = 0
+        for a, b in zip(nums, freq):
+            ans = (ans + a * b) % MOD
+
+        return ans
