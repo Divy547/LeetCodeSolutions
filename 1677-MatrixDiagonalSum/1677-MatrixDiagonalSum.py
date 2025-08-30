cc18651080f1a91d676c6e49d2eb94411442fea0
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-08-30 23:52:57 +0530

    Create 1677-MatrixDiagonalSum

---
+++ 1677-MatrixDiagonalSum/1677-MatrixDiagonalSum.py
@@ -0,0 +1,9 @@
+# Last updated: 8/30/2025, 11:52:51 PM
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        diagSum = 0
+        for i in range(0, len(mat)):
+            for j in range(0, len(mat)):
+                if i == j or i + j == len(mat) - 1:
+                    diagSum += mat[i][j]
+        return diagSum
