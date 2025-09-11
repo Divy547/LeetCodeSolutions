8ede6d0b764fd4ed4bd089aa9fda53ffbe8f96e3
Divy Akash Gupta <divyakaskgupta@gmail.com>
2025-09-11 16:14:05 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.cpp
@@ -0,0 +1,32 @@
+// Last updated: 9/11/2025, 4:14:03 PM
+#include <stack>
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> chars;
+        int top = -1;
+        for (char ch : s) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                chars.push(ch);
+            } else {
+                if (chars.empty()) {
+                    return false;
+                }
+                char top = chars.top();
+                chars.pop();
+                if (ch == ')' && top != '(') {
+                    
+                    return false;
+                }
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+            }
+        
+        }
+    return chars.empty();
+    }
+};
