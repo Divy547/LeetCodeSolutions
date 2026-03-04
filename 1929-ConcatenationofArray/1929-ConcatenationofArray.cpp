dce5c6bd30473f8b25e565858325dd52b78f64c1
Divy Akash Gupta <divyakaskgupta@gmail.com>
2026-03-04 19:29:18 +0530

    File updated at 3/4/2026, 7:29:17 PM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
@@ -1,16 +1,16 @@
-// Last updated: 3/4/2026, 7:26:03 PM
+// Last updated: 3/4/2026, 7:29:17 PM
 1class Solution {
 2public:
-3    vector<int> shuffle(vector<int>& nums, int n) {
-4        vector<int> ans;
-5        int i = 0, j = n;
-6        while(i < n && j < 2*n){
-7            ans.push_back(nums[i]);
-8            ans.push_back(nums[j]);
-9            i++;
-10            j++;
-11        }
-12        return ans;
-13
+3    int findMaxConsecutiveOnes(vector<int>& nums) {
+4        int maxNums = 0, curr = 0;
+5        for(int x: nums){
+6            if(x == 1){
+7                curr++;
+8            }else{
+9                curr = 0;
+10            }
+11            maxNums = max(curr, maxNums);
+12        }
+13        return maxNums;
 14    }
 15};
