5f4f4598d0f3fe9dfd9f7025b5a6b8df09556cb3
Divy Akash Gupta <divyakaskgupta@gmail.com>
2026-03-22 22:52:48 +0530

    File updated at 3/22/2026, 10:52:40 PM

--- 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.cpp
+++ 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.cpp
@@ -1,19 +1,23 @@
-// Last updated: 3/22/2026, 10:11:23 PM
+// Last updated: 3/22/2026, 10:52:40 PM
 1class Solution {
 2public:
 3    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-4        // nums.sort();
-5        // vector<int> buff;
+4        vector<int> copy = nums;
+5        sort(nums.begin(), nums.end());
 6        vector<int> ans;
-7        for(int i = 0; i < nums.size(); i++){
-8            int count = 0;
-9            for(int j = 0; j < nums.size(); j++){
-10                if(nums[i] > nums[j]){
-11                    count++;
-12                }
-13            }
-14            ans.push_back(count);
-15        }
-16        return ans;
-17    }
-18};
+7        unordered_map<int, int> hashMap;
+8        for(int i = 0; i < nums.size(); i++){
+9            if(hashMap.count(nums[i])){
+10            }else{
+11                hashMap.insert({nums[i], i});
+12            }
+13        }
+14        for(auto it = hashMap.begin(); it != hashMap.end(); it++)
+15            cout << it->first << ": " << it->second
+16            << endl;
+17        for(int i = 0; i < copy.size(); i++){
+18            ans.push_back(hashMap[copy[i]]);
+19        }
+20        return ans;
+21    }
+22};
