d9c965b73b75756b9eb2d61641ff8998c82bc1c1
Divy Akash Gupta <divyakaskgupta@gmail.com>
2026-03-22 23:45:10 +0530

    File updated at 3/22/2026, 11:45:02 PM

--- 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.cpp
+++ 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.cpp
@@ -1,23 +1,19 @@
-// Last updated: 3/22/2026, 10:52:40 PM
+// Last updated: 3/22/2026, 11:45:02 PM
 1class Solution {
 2public:
-3    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-4        vector<int> copy = nums;
-5        sort(nums.begin(), nums.end());
+3    vector<int> findDisappearedNumbers(vector<int>& nums) {
+4        sort(nums.begin(), nums.end());
+5        // v.erase(remove(v.begin(), v.end(), 2), v.end());
 6        vector<int> ans;
-7        unordered_map<int, int> hashMap;
-8        for(int i = 0; i < nums.size(); i++){
-9            if(hashMap.count(nums[i])){
-10            }else{
-11                hashMap.insert({nums[i], i});
-12            }
-13        }
-14        for(auto it = hashMap.begin(); it != hashMap.end(); it++)
-15            cout << it->first << ": " << it->second
-16            << endl;
-17        for(int i = 0; i < copy.size(); i++){
-18            ans.push_back(hashMap[copy[i]]);
-19        }
-20        return ans;
-21    }
-22};
+7
+8        for (int i = 0; i < nums.size(); i++) {
+9            nums[abs(nums[i]) - 1] = abs(nums[abs(nums[i]) - 1]) * -1;
+10        }
+11        for (int i = 0; i < nums.size(); i++) {
+12            if (nums[i] > 0) {
+13                ans.push_back(i+1);
+14            }
+15        }
+16        return ans;
+17    }
+18};
